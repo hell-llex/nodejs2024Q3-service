25d7548adc6a25a7f012726ccc83f757e6f5d0fc
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-11-25 00:58:04 +0300

    fix: fix track

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -11,21 +11,25 @@ import {
   ParseUUIDPipe,
   NotFoundException,
   BadRequestException,
+  UseGuards,
 } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { CreateTrackDto, UpdateTrackDto } from './dto/track.dto';
 import { Track } from './interfaces/track.interface';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('track')
 export class TrackController {
   constructor(private readonly trackService: TrackService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   @HttpCode(HttpStatus.OK)
   async getAllTrack() {
     return await this.trackService.getAllTrack();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   async getTrackById(
@@ -45,6 +49,7 @@ export class TrackController {
     return await this.trackService.getTrackById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   @HttpCode(HttpStatus.CREATED)
   async createTrack(@Body() createTrackDto: CreateTrackDto): Promise<Track> {
@@ -57,6 +62,7 @@ export class TrackController {
     );
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   async updateTrack(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -69,6 +75,7 @@ export class TrackController {
     return await this.trackService.updateTrack(id, updateTrackDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteTrack(
--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, forwardRef(() => AuthModule)],
   controllers: [TrackController],
   providers: [TrackService],
 })
